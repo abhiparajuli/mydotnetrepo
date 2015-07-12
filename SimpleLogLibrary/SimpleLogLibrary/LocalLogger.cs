c99e0e8293d4a2b953e8bf4497713d6f46b8435e
abhiparajuli <abhi.parajuli@gmail.com>
2015-07-12 14:34:06 -0400

    Initial Commit of the SimpleLogLibrary

---
+++ SimpleLogLibrary/SimpleLogLibrary/LocalLogger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SimpleLogLibrary
+{
+    public class LocalLogger
+    {
+        public enum LogLevel { LOG_NONE, LOG_ERROR, LOG_WARNING, LOG_DEBUG };
+        private LogLevel localLogLevel;
+        private String logFilePath;
+
+        public LocalLogger()
+        {
+            this.localLogLevel = LogLevel.LOG_ERROR;
+            this.logFilePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), string.Format("LocalLoggerFile{0}.log", getTotalMillisecondsUTC()));
+        }
+
+        /// <summary>
+        /// Overloaded constructor that takes in log level to set and a file to write to
+        /// </summary>
+        /// <param name="localLogLevel"></param>
+        /// <param name="logFilePath"></param>
+        public LocalLogger(LogLevel localLogLevel, String logFilePath)
+        {
+            this.localLogLevel = localLogLevel;
+            if (!String.IsNullOrEmpty(logFilePath))
+            {
+                this.logFilePath = logFilePath;
+            }
+            else
+            {
+                this.logFilePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), string.Format("LocalLoggerFile{0}.log", getTotalMillisecondsUTC()));
+            }
+
+        }
+
+        /// <summary>
+        /// helper method to generate a timestamp unique to the millisecond
+        /// </summary>
+        /// <returns></returns>
+        private long getTotalMillisecondsUTC()
+        {
+            DateTime StartOfEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+            TimeSpan mySpan = DateTime.UtcNow - StartOfEpoch;
+            return (long)mySpan.TotalMilliseconds;
+        }
+
+
+        /// <summary>
+        /// log method to be called for logging messages at a given log level
+        /// </summary>
+        /// <param name="msgLogLevel"></param>
+        /// <param name="logText"></param>
+        public void log(LogLevel msgLogLevel, String logText)
+        {
+            StringBuilder formattedLogText = new StringBuilder();
+            try
+            {
+                formattedLogText.Append(DateTime.Now.ToShortDateString());
+                formattedLogText.Append(" ");
+                formattedLogText.Append(DateTime.Now.ToLongTimeString());
+
+                if (msgLogLevel <= localLogLevel)
+                {
+                    formattedLogText.Append("   ");
+                    formattedLogText.AppendLine(logText);
+                    fileLogger(formattedLogText.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("[{0}       Error executing log method - {1}", formattedLogText, ex.ToString()));
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// helper method to help log method write to file
+        /// </summary>
+        /// <param name="strMessage"></param>
+        private void fileLogger(String strMessage)
+        {
+            File.AppendAllText(logFilePath, strMessage);
+        }
+
+    }
+}
---
+++ SimpleLogLibrary/TestSimpleLogLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿using SimpleLogLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSimpleLogLibrary
+{
+    class Program
+    {
+        /// <summary>
+        /// test harness for the simple log library
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            LocalLogger.LogLevel setLevel = LocalLogger.LogLevel.LOG_DEBUG;
+            if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "0":
+                        setLevel = LocalLogger.LogLevel.LOG_NONE;
+                        break;
+                    case "1":
+                        setLevel = LocalLogger.LogLevel.LOG_ERROR;
+                        break;
+                    case "2":
+                        setLevel = LocalLogger.LogLevel.LOG_WARNING;
+                        break;
+                    case "3":
+                        setLevel = LocalLogger.LogLevel.LOG_DEBUG;
+                        break;
+                }
+            }
+            LocalLogger myLogger = new LocalLogger(setLevel, null);
+            testMessages(myLogger);
+        }
+
+        /// <summary>
+        /// helper method for the log library test harness
+        /// </summary>
+        /// <param name="myLogger"></param>
+        private static void testMessages(LocalLogger myLogger)
+        {
+            myLogger.log(LocalLogger.LogLevel.LOG_DEBUG, "My test debug message.");
+            myLogger.log(LocalLogger.LogLevel.LOG_WARNING, "My test warning message.");
+            myLogger.log(LocalLogger.LogLevel.LOG_ERROR, "My test error message.");
+            myLogger.log(LocalLogger.LogLevel.LOG_NONE, "My test none message.");
+        }
+    }
+}
